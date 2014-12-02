90e813623a57332cf1c5b852ef2aa9e1592e5dc7
Anton Anisimov <masterpersi@gmail.com>
2014-12-02 10:28:55 +0600

    footer + image

---
+++ main.js
@@ -0,0 +1,65 @@
+
+$(function () {
+
+//    $(document).snowfall(
+//{
+//    flakeCount: 150,        // number
+//    flakeColor: '#fff', // string
+//    flakeIndex: 999999,     // number
+//    minSize: 1,            // number
+//    maxSize: 3,            // number
+//    minSpeed: 2,           // number
+//    maxSpeed: 10,           // number
+//    round: true,          // bool
+//    shadow: false          // bool
+//}
+//);
+
+    var $toy = $('.toy');
+    var $body = $('body');
+    var $borderProductTpl = $("<div class=\"productBorder\"><img id=\"theImg\" /></div>");
+    var $productImg = $borderProductTpl.children('img');
+    $body.append($borderProductTpl);
+
+    var randomAnimateClasses = ['zoomIn', 'flipInY', 'rotateIn', 'bounce', 'swing', 'bounceIn', 'wobble', 'tada'];
+
+    $body.on('click', function (e) {
+        //if(!$(e.target).hasClass('.toy'))
+        //    $borderProductTpl.hide();
+    });
+
+    $toy.on('click', function (e) {
+        var $this = $(this);
+
+
+        $productImg.attr('src', 'images/products/product' + $this.data('toyid') + '.jpg');
+
+        var productFrameOffset = $.extend({}, $this.offset());
+        productFrameOffset.left -= 30;
+        productFrameOffset.top -= 30;
+        $borderProductTpl
+            .offset(productFrameOffset);
+
+        $borderProductTpl.show();
+        var animateEffect = randomAnimateClasses[randomIntFromInterval(0, randomAnimateClasses.length-1)];
+        $borderProductTpl.addClass('animated ' + animateEffect);
+        $borderProductTpl.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $borderProductTpl.removeClass('animated ' + animateEffect);
+
+        });
+    });
+
+    $toy.on('mouseover', function (e) {
+        $(this).addClass('animated swing');
+    })
+    .on('mouseout', function (e) {
+        $(this).removeClass('animated swing');
+    });
+
+
+});
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
